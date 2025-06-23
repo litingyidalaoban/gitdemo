b5ba544005a06573f61935cfd07924fc2507c84c
litingyi2232-admin <litingyidalaoban@gmail.com>
2025-06-23 20:16:51 +0800

    在args的代码上新增了注释

--- tools/train.py
+++ tools/train.py
@@ -9,7 +9,10 @@ from mmmengine.utils import DictAction
 
 #如果是两个横杠
 def parse_args():
+    #创建一个args的解析的对象，所有的args都会绑定到他上面去
     parser = argparse.ArgumentParser(description='Train a segmentor')
+    #添加声明，比如config，help就是帮助信息
+    #action=storetrue就是当这个参数有输入的时候就是True，不然模型只有输入True才行，一定要严格
     parser.add_argument('config', help='train config file path')
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
@@ -22,6 +25,7 @@ def parse_args():
         action='store_true',
         default=False,
         help='enable automatic-mixed-precision training')
+    #这个nargs就是如果你用了--cfg-options，你就得至少输入一个，然后变成列表
     parser.add_argument(
         '--cfg-options',
         nargs='+',
@@ -32,6 +36,7 @@ def parse_args():
         'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
         'Note that the quotation marks are necessary and that no white space '
         'is allowed.')
+    #这个就是只能从里面选一个，不然报错，或者默认就是none
     parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
@@ -40,7 +45,9 @@ def parse_args():
     # When using PyTorch version >= 2.0.0, the `torch.distributed.launch`
     # will pass the `--local-rank` parameter to `tools/train.py` instead
     # of `--local_rank`.
+    #允许两种不同的名称，但是限定输入为整数类型，默认是0
     parser.add_argument('--local_rank', '--local-rank', type=int, default=0)
+    
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
